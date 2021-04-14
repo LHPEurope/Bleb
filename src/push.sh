60e1fa216e9ffb58007bfc490d8c8809fedd5c24
damianodantuonolhp <damiano.dantuono@lhpes.com>
2021-04-14 15:13:08 +0200

    push

--- src/push.sh
+++ src/push.sh
@@ -6,3 +6,8 @@ git add ./info
 git commit ./logs -m "new data"
 git commit ./info -m "new info"
 git push -f
+
+cd /home/pi/Source/RubbiTools/
+git add ./DataCollection/batteries
+git commit ./DataCollection/batteries -m "new batteries"
+git push -f
